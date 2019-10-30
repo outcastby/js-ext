dfa495a0fa43c7118d5630c0b83f241c734f744f
Mikhail Kruglik <miheykrug@mail.ru>
2019-10-30 20:14:01 +0300

    Test import

--- build/index.js
+++ build/index.js
@@ -1,15 +1,5 @@
-const Redux = require('./utils/redux');
+const redux = require('./utils/redux').default;
 
-Ext = {
-  Redux,
+module.exports = {
+  redux,
 }
-
-const Ext$2 = Object.freeze({
-  default: Ext
-});
-
-const Ext$3 = ( Ext$2 && Ext ) || Ext$2;
-
-const react = Ext$3.default || Ext$3;
-
-module.exports = react
