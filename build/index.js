324ed07045c94fc289452a7da741e00a8feb2b02
Mikhail Kruglik <miheykrug@mail.ru>
2019-10-30 21:48:52 +0300

    Test import

---
+++ build/index.js
@@ -0,0 +1,9 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const redux_1 = __importDefault(require("./utils/redux"));
+exports.default = {
+    redux: redux_1.default,
+};
