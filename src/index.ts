7ce34c4c266d0ed89c11d6d2b1ec63c63e42608f
atanych <atanych@gmail.com>
2019-11-03 18:37:57 +0300

    Add FP

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { default as Redux } from './redux'
+export { default as fp } from './utils/fp'
 
 export { default as SwitchRoutes } from './components/SwitchRoutes'
---
+++ src/utils/fp.ts
@@ -0,0 +1,5 @@
+import { get } from 'lodash'
+import { set } from 'lodash/fp'
+
+export const getIn = get
+export const setIn = (object: any, path: any, value: any): any => set(path, value, object)
