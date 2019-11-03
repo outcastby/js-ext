7756e7a06fe265280a4b0b9764face2d7e0e6cb7
atanych <atanych@gmail.com>
2019-11-03 18:38:42 +0300

    v0.0.18

--- src/utils/fp.ts
+++ src/utils/fp.ts
@@ -1,5 +1,10 @@
 import { get } from 'lodash'
 import { set } from 'lodash/fp'
 
-export const getIn = get
-export const setIn = (object: any, path: any, value: any): any => set(path, value, object)
+const getIn = get
+const setIn = (object: any, path: any, value: any): any => set(path, value, object)
+
+export default {
+  getIn,
+  setIn,
+}
