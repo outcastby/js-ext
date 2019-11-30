78681ea82b3b186e0ec06f313498e0090b724fdd
atanych <atanych@gmail.com>
2019-11-30 23:57:01 +0300

    Add absolute paths

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,15 @@
-export { default as Redux } from './redux'
-export { default as Router } from './router'
-export { default as fp } from './utils/fp'
-export { default as GQL } from './core/gql'
-export { default as Cookies } from './utils/cookies'
-export { default as LocalStorage } from './utils/LocalStorage'
-export { default as Config } from './config'
+export { default as Redux } from 'redux'
+export { default as Router } from 'router'
+export { default as fp } from 'utils/fp'
+export { default as GQL } from 'core/gql'
+export { default as Cookies } from 'utils/cookies'
+export { default as LocalStorage } from 'utils/LocalStorage'
+export { default as Config } from 'config'
 
-export { default as SwitchRoutes } from './router/components/SwitchRoutes'
+export { default as SwitchRoutes } from 'router/components/SwitchRoutes'
 
-export { default as Modal } from './components/Modal'
+export { default as Modal } from 'components/Modal'
 
 // interfaces
-export { Action } from './redux/interfaces'
-export { default as Dictionary } from './interfaces/Dictionary'
+export { Action } from 'redux/interfaces'
+export { default as Dictionary } from 'interfaces/Dictionary'
